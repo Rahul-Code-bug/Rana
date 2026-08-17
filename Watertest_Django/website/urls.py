6efe50be058445f07c339d2bf8efd1f98fc3d27b
Rahuluf <rsharma38045@gmail.com>
2026-08-17 11:27:16 +0530

    done

--- Watertest_Django/website/urls.py
+++ Watertest_Django/website/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path(
+        "laboratory-methods/",
+        views.laboratory_methods,
+        name="laboratory_methods",
+    ),
 ]
--- Watertest_Django/website/views.py
+++ Watertest_Django/website/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "website/home.html")
+
+
+def laboratory_methods(request):
+    return render(request, "website/laboratory_methods.html")
