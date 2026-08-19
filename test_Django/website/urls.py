ebeff9d5e88becf54139bc0ccfaed4eb943dea41
Rahuluf <rsharma38045@gmail.com>
2026-08-19 11:56:29 +0530

    done

---
+++ test_Django/website/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path(
+        "laboratory-methods/",
+        views.laboratory_methods,
+        name="laboratory_methods",
+    ),
+]
--- test_Django/website/views.py
+++ test_Django/website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, "website/home.html")
+
+
+def laboratory_methods(request):
+    return render(request, "website/laboratory_methods.html")
